4f9c5ce9bf1ebb9ee378c64062edc6115e08e581
Jules GAYET <jules.gayet12@gmail.com>
2025-05-07 14:19:48 +0200

    Add LaticeApplicationConsole

---
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -0,0 +1,16 @@
+package latice.application;
+
+public class LaticeApplicationConsole {
+	
+	public static void main(String[] args) {
+		System.out.println("-------------------------------------------------------");
+		System.out.println("--  Bienvenue dans notre magnifique jeu de latice !  --");
+		System.out.println("--  Develloppé par Jules		     	     --");	
+		System.out.println("--  et par Mostapha				     --");
+		System.out.println("--  et par Ahmed				     --");
+		System.out.println("--  et par Milan				     --");
+		System.out.println("-------------------------------------------------------");
+	}  
+	
+
+}
