506617c277f41cea61f9a50ac1fcad9f45098473
Milan LOI <milan.loi@etu.unilim.fr>
2025-05-13 18:18:41 +0200

    pom xml and javafx

--- src/main/java/latice/view/Main.java
+++ src/main/java/latice/view/Main.java
@@ -1,5 +1,7 @@
 package latice.view;
 
+import java.net.URL;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -10,10 +12,17 @@ public class Main extends Application{
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		Parent root = FXMLLoader.load(getClass().getResource("GraphicBoard.fxml"));
-		primaryStage.setTitle("Jeu Latice");
-		primaryStage.setScene(new Scene(root));
-		primaryStage.show();
+		try {
+			URL fxmlLocation = getClass().getResource("/GraphicBoard.fxml");
+			System.out.println("FXML Location: " + fxmlLocation); // doit être non null
+			Parent root = FXMLLoader.load(fxmlLocation);
+
+			primaryStage.setTitle("Jeu Latice");
+			primaryStage.setScene(new Scene(root));
+			primaryStage.show();
+		} catch (Exception e) {
+	        e.printStackTrace();
+		}
 	}
 	
 	public static void main(String[] args) {
