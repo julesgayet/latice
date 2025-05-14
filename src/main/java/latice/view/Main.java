166132a8c2c5fddc34c1ccaa628526f44e066a16
Jules GAYET <jules.gayet12@gmail.com>
2025-05-14 11:25:54 +0200

    Add setResizable(false) in Main

--- src/main/java/latice/view/Main.java
+++ src/main/java/latice/view/Main.java
@@ -8,24 +8,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application{
-	
-	@Override
-	public void start(Stage primaryStage) throws Exception{
-		try {
-			URL fxmlLocation = getClass().getResource("/GraphicBoard.fxml");
-			System.out.println("FXML Location: " + fxmlLocation); // doit être non null
-			Parent root = FXMLLoader.load(fxmlLocation);
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        try {
+            URL fxmlLocation = getClass().getResource("/GraphicBoard.fxml");
+            System.out.println("FXML Location: " + fxmlLocation);
+            Parent root = FXMLLoader.load(fxmlLocation);
 
-			primaryStage.setTitle("Jeu Latice");
-			primaryStage.setScene(new Scene(root));
-			primaryStage.show();
-		} catch (Exception e) {
-	        e.printStackTrace();
-		}
-	}
+            primaryStage.setTitle("Jeu Latice");
+            primaryStage.setScene(new Scene(root));
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+            primaryStage.setResizable(false);
+
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
