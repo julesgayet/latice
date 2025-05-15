690d2e5ed6801d5afd3989c7ff5ac0e7e2b3e26b
Ahmed MESRI <ahmed.mesri@etu.unilim.fr>
2025-05-15 09:14:54 +0200

    add testFirstPlayerReturnsEitherP1OrP2() relating to Game.firstPlayer()

--- src/test/java/latice/model/GameTest.java
+++ src/test/java/latice/model/GameTest.java
@@ -60,9 +60,11 @@ public class GameTest {
     @Test
     public void testFillRack() {
         // Add 3 tiles to player1 deck
-        List<Tile> deck = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            deck.add(new Tile(i, Color.RED, Symbol.FEATHER, false));
+        List<Tile> rack = new ArrayList<>();
+        while (rack.size() < 5) {
+        	int i = 0;
+            rack.add(new Tile(i, Color.RED, Symbol.FEATHER, false));
+            i = i + 1;
         }
         player1.Deck(deck);
 
@@ -101,4 +103,23 @@ public class GameTest {
         assertEquals(game.getCurrentPlayer(), game.getWinner());
     }
     
+    @Test
+    public void testFirstPlayerReturnsEitherP1OrP2() {
+        Game game = new Game(player1, player2, board, true);
+        Player p1 = new Player("Ahmed", null, null, null, null);
+        Player p2 = new Player("Mostapha", null, null, null, null);
+
+        boolean p1Chosen = false;
+        boolean p2Chosen = false;
+
+        for (int i = 0; i < 10; i++) {
+            Player first = game.firstPlayer(p1, p2);
+            if (first == p1) p1Chosen = true;
+            if (first == p2) p2Chosen = true;
+        }
+
+        assertTrue(p1Chosen, "p1 should be chosen at least once");
+        assertTrue(p2Chosen, "p2 should be chosen at least once");
+        
+    }
 }
