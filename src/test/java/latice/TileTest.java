eaf635751c17dc459d718bea406d8c675068ef5b
Mostapha Ayeb <ayebmostapha@gmail.com>
2025-05-20 15:28:38 +0200

    update tileTest

--- src/test/java/latice/TileTest.java
+++ src/test/java/latice/TileTest.java
@@ -12,40 +12,39 @@ import latice.model.tiles.Tile;
 
 public class TileTest {
 
-	@Test
-	public void testConstructor() {
-	    Color color = Color.RED;
-	    Symbol symbol = Symbol.TURTLE;
-	    Tile tile = new Tile(1, color, symbol);
-	    
-	    assertEquals(1, tile.getId());
-	    assertEquals(color, tile.getColor());
-	    assertEquals(symbol, tile.getSymbol());
-	    assertTrue(tile.getInGame());
-	}
+    @Test
+    public void testConstructor() {
+        Color color = Color.RED;
+        Symbol symbol = Symbol.TURTLE;
+        Tile tile = new Tile(1, color, symbol);
+        
+        assertEquals(1, tile.getId());
+        assertEquals(color, tile.getColor());
+        assertEquals(symbol, tile.getSymbol());
+    }
 
-	@Test
-	public void testGetters() {
-	    Tile tile = new Tile(5, Color.PINK, Symbol.DOLPHIN);
-	    
-	    assertEquals(Integer.valueOf(5), tile.getId());
-	    assertEquals(Color.PINK, tile.getColor());
-	    assertEquals(Symbol.DOLPHIN, tile.getSymbol());
-	    assertFalse(tile.getInGame());
-	}
-	
-	@Test
-	public void testSetters() {
-	    Tile tile = new Tile(0, Color.GREEN, Symbol.FEATHER);
-	    tile.Id(99);
-	    tile.Color(Color.YELLOW);
-	    tile.Symbol(Symbol.SEAGULL);
-	    tile.InGame(false);
+    @Test
+    public void testGetters() {
+        Tile tile = new Tile(5, Color.PINK, Symbol.DOLPHIN);
+        
+        assertEquals(Integer.valueOf(5), tile.getId());
+        assertEquals(Color.PINK, tile.getColor());
+        assertEquals(Symbol.DOLPHIN, tile.getSymbol());
+        assertFalse(tile.getInGame());
+    }
+    
+    @Test
+    public void testSetters() {
+        Tile tile = new Tile(0, Color.GREEN, Symbol.FEATHER);
+        tile.Id(99);
+        tile.Color(Color.YELLOW);
+        tile.Symbol(Symbol.SEAGULL);
+        tile.InGame(false);
 
-	    assertEquals(Integer.valueOf(99), tile.getId());
-	    assertEquals(Color.YELLOW, tile.getColor());
-	    assertEquals(Symbol.SEAGULL, tile.getSymbol());
-	    assertFalse(tile.getInGame());
-	}
+        assertEquals(Integer.valueOf(99), tile.getId());
+        assertEquals(Color.YELLOW, tile.getColor());
+        assertEquals(Symbol.SEAGULL, tile.getSymbol());
+        assertFalse(tile.getInGame());
+    }
 
 }
